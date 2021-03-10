a2896fdf8ad26464e85cfd32acf6eac5e3af270f
linda karmouche <lindakarmouche@yahoo.fr>
2021-03-10 15:13:58 +0100

    update sur model user et view tools

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
  has_many :tools
   #Include default devise modules. Others available are:
   #:confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- # devise :database_authenticatable, :registerable,
-      #:recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :validatable
 validates :email, presence: true, uniqueness: true
 end
 
