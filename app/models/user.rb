ebf96efa9941998244aa9077a7088f61a4f57c02
linda karmouche <lindakarmouche@yahoo.fr>
2021-03-10 12:37:46 +0100

    seedlinda

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
+## Code Linda ##
 class User < ApplicationRecord
+ has_many :tools
+  #Include default devise modules. Others available are:
+  #:confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+ # devise :database_authenticatable, :registerable,
+      #:recoverable, :rememberable, :validatable
+validates :email, presence: true, uniqueness: true
+end
+
+## Code Equipe ##
+#class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :email, with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/, presence: true, uniqueness: true
-end
+  #devise :database_authenticatable, :registerable,
+        # :recoverable, :rememberable, :validatable
+  # validates :email, with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/, presence: true, uniqueness: true
+#end
