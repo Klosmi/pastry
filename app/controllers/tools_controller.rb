20efca7b8f69e134aececa54d45d6edf57cc8e3a
lindaounissi <linda.ounissi020712@gmail.com>
2021-03-10 12:25:45 +0100

    test2 from new branch

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,25 @@
+class ToolsController < ApplicationController
+
+  # Get/tools
+  def index
+    @tool = Tool.find(params[:id])
+    @tools = Tool.all
+  end
+
+  # Get/tools/1
+
+  def show
+
+    @tool = Tool.new
+  end
+
+  # private
+
+  # def set_tool
+  #   @tool = Tool.find(params[:id])
+  # end
+
+   def params
+     params.require(:tool).permit(:name, :details, :price_by_day, :picture, :category, :user_id)
+   end
+end
